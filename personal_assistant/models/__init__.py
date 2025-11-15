8eae851805dc116ea1407bf28d315c3bc9610170
Ovavova <vyereschenko@gmail.com>
2025-11-15 14:34:12 +0200

    added unit tests for Email class

--- personal_assistant/models/__init__.py
+++ personal_assistant/models/__init__.py
@@ -1,4 +1,4 @@
-from personal_assistant.models.field import Field, Name, Birthday, Phone, Title, Tag
+from personal_assistant.models.field import Field, Name, Birthday, Phone, Title, Tag, Email
 from personal_assistant.models.record import Record
 from personal_assistant.models.address_book import AddressBook
 from personal_assistant.models.note import Note
